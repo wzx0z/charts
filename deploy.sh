cdcb0afb97d8639374abb84325cdfb019f979f30
wuzhixiang <wuzhixiang@eccom.com.cn>
2021-03-02 13:25:15 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
-git ls-tree -r --name-only HEAD | while read filename; do
-  unixtime=$(git log -1 --format="%at" -- "${filename}")
-  echo -t ${touchtime} "${filename}"
+git diff-tree --no-commit-id --name-status -r master | while read status filename; do
+  if [[ $status -eq "M" ]]; then
+    echo "Copy $filename to S3"
+  elif [[ $status -eq "D" ]]; then
+    echo "Delete $filename from S3"
+  else
+    echo "Skip"
+  fi
 done
